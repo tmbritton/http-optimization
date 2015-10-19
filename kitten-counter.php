d1677ad04a9c428ed195ee3cb57c0227f7dc2812
Thomas Britton <tom.britton@wholefoods.com>
2015-10-19 16:30:47 -0500

    Domain sharding

--- kitten-counter.php
+++ kitten-counter.php
@@ -17,8 +17,20 @@ foreach($out as $image_data) {
     $classes .= ' width2';
     $path = 'medium';
   }
+  if ($rando <= 3) {
+    $shard = 'shard1';
+  }
+  elseif ($rando == 4 || $rando == 5) {
+    $shard = 'shard2';
+  }
+  elseif ($rando == 6 || $rando == 7) {
+    $shard = 'shard3';
+  }
+  elseif ($rando >= 8) {
+    $shard = 'shard4';
+  }
   $html = '<a href="images/large/' . $image_data['FileName'] . '" data-lightbox="kittens" data-title="' . $image_data['FileName'] . '">' . "\n";
-  $html .= "\t" . '<img class="' . $classes . '" src="images/' . $path . '/' . $image_data['FileName'] . '" alt="">' . "\n";
+  $html .= "\t" . '<img class="' . $classes . '" src="//' . $shard . '.http-1-demo.tmbritton.com/images/' . $path . '/' . $image_data['FileName'] . '" alt="">' . "\n";
   $html .= '</a>' . "\n";
 
   echo $html;
